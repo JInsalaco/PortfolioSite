9ec2d30d3d30632f5e0cecde9fb2e538f668df04
Joseph Insalaco <joseph.insalaco1@gmail.com>
2022-05-27 18:01:14 -0400

    Added favicon images for all platforms

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,8 +2,8 @@ import { Container} from "react-bootstrap";
 
 export function Welcome() {
     return(
-        <Container className="mb-4">
-            <div id='home' className='text-center'>
+        <Container id='home' className="mb-4">
+            <div className='text-center'>
                 <h1>Welcome to my site!</h1>
                 <p>Welcome to the personal website of Joseph Insalaco</p>
             </div>
