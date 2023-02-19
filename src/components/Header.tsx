aa9dc815a29ee47481270ee4ed071a8926c1171b
Joseph Insalaco <joseph.insalaco1@gmail.com>
2023-02-19 01:18:50 -0500

    Updating packages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { AppBar, Toolbar, Button, CssBaseline } from '@mui/material';
 import { Link as RouterLink } from "react-router-dom";
 import { makeStyles } from "tss-react/mui";
-import headshot from './headshot.jpeg'
 
 const useStyles = makeStyles()(() => ({
     header: {
