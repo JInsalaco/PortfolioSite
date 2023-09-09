d110475c5534ca77850adb3dd6692f855b1db329
Joseph Insalaco <joseph.insalaco1@gmail.com>
2023-09-09 13:21:03 -0400

    Deleting unused files

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export function Footer() {
-    return(
-        <footer>
-            <Typography variant="body2" justifyContent={"center"}>© Joseph Insalaco 2023</Typography>
-        </footer>
-    )
-}
