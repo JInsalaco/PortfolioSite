40c1da56898bf07fa38dc5bee3adbbca13dc8acf
Joseph insalaco <joseph.insalaco1@gmail.com>
2024-02-16 18:12:52 -0500

    Adding responsive navbar for better mobile experience (I did not have enough tabs before this to cause issues)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Box, IconButton, Typography} from '@mui/material';
+import { AppBar, Toolbar, Button, Box, IconButton, Typography, Menu, MenuItem} from '@mui/material';
 import { Link as RouterLink } from "react-router-dom";
 import { useTheme } from '@mui/material/styles';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import MenuIcon from '@mui/icons-material/Menu';
 import { ColorModeContext } from '../ColorModeContext';
 const menuButtons: {href: string, title: string}[] = [
     {
@@ -23,6 +24,15 @@ const menuButtons: {href: string, title: string}[] = [
 export default function Header() {
     const theme = useTheme();
     const colorMode = React.useContext(ColorModeContext);
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
     const showMenuButtons = () => {
          return menuButtons.map(({title, href}) => {
@@ -70,10 +80,49 @@ export default function Header() {
     return(
         <AppBar sx={{mb:5}} position="sticky">
             <Toolbar sx={{justifyContent: 'flex-end'}}>
+                <Box sx={{display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                    }}
+                >
+                {menuButtons.map(({title, href}) => (
+                    <MenuItem key={title} onClick={handleCloseNavMenu}>
+                        <Typography component="a" href='/' textAlign="center">{title}</Typography>
+                    </MenuItem>
+                ))}
+                </Menu>
+                </Box>
                 <Typography variant="h6" component="a" href='/' sx={{ flexGrow: 1, color: 'inherit', textDecoration: 'none' }}>
                     Joseph Insalaco
                 </Typography>
-                {showMenuButtons()}
+                <Box sx={{display: { xs: 'none', md: 'flex' } }}>
+                    {showMenuButtons()}
+                </Box>
+
                 {themeToggle()}
             </Toolbar>
         </AppBar>
