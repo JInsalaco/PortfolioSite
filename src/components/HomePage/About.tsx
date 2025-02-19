727ea425c3a418dc651eb16c91d182905c3a9c5a
Joseph insalaco <joseph.insalaco1@gmail.com>
2025-02-18 22:33:09 -0500

    Updating job

--- src/components/HomePage/About.tsx
+++ src/components/HomePage/About.tsx
@@ -8,7 +8,7 @@ export function About(){
             </Typography>
             <Card sx={{p: 2}}>
                 <p>
-                    I am a full-stack web developer based in New York City and currently working at Bank of America. I have over 5 years of experience in React, Node.js, Java, MongoDB, Express, Redis, and AWS.
+                    I am a full-stack web developer based in New York City and currently work at JPMorgan Chase. I have over 5 years of experience in React, Java, Typescript, PostgreSQL, MongoDB, and AWS.
                 </p>
                 <p>
                     Please check out <Link href="/resume">my resume</Link> for more information on my skillset and work projects.
